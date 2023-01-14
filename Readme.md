507651cc7a17a56664c3e8871813b990d76c9b02
Ramit Sarkar <ramitsarkar@Ramits-MacBook-Air.local>
2023-01-14 14:19:08 +0530

    added readme

---
+++ Readme.md
@@ -0,0 +1,7 @@
+cd to server and run the following commands
+npm install
+npm start
+
+cd to client and run the following commands-
+yarn install
+yarn start
--- client/src/apiCalls.js
+++ client/src/apiCalls.js
@@ -1,31 +1,51 @@
 import axios from "axios";
 
 export const getAllTransactions = async (setData) => {
-  const res = await axios.get(
-    "http://localhost:10000/api/user/all/transaction"
-  );
-  setData(res.data);
+  try {
+    const res = await axios.get(
+      "http://localhost:10000/api/user/all/transaction"
+    );
+    setData(res.data);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 export const getATransaction = async (id, setData) => {
-  const res = await axios.get(
-    `http://localhost:10000/api/user/transaction/${id}`
-  );
-  setData(res.data);
+  try {
+    const res = await axios.get(
+      `http://localhost:10000/api/user/transaction/${id}`
+    );
+    setData(res.data);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 export const deleteTransaction = async (id) => {
-  await axios.delete(`http://localhost:10000/api/user/transaction/${id}`);
+  try {
+    await axios.delete(`http://localhost:10000/api/user/transaction/${id}`);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 export const getAllUsers = async (setUserData) => {
-  const res = await axios.get("http://localhost:10000/api/user");
-  setUserData(res.data);
+  try {
+    const res = await axios.get("http://localhost:10000/api/user");
+    setUserData(res.data);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 export const updateTransaction = async ({ id, userId, amount }) => {
-  await axios.put(`http://localhost:10000/api/user/transaction/${id}`, {
-    user: userId,
-    amount: amount,
-  });
+  try {
+    await axios.put(`http://localhost:10000/api/user/transaction/${id}`, {
+      user: userId,
+      amount: amount,
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
 };
